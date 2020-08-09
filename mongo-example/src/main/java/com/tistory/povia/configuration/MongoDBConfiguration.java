c04c4501c68930aeb00d525deef9f14b4905cc9c
povia <tjddn020@naver.com>
2020-08-10 01:15:24 +0900

    [SW] initial update

---
+++ mongo-example/src/main/java/com/tistory/povia/configuration/MongoDBConfiguration.java
@@ -0,0 +1,2 @@
+package com.tistory.povia.configuration;
+
---
+++ mongo-example/src/main/java/com/tistory/povia/mongo/dto/PersonDto.java
@@ -0,0 +1,11 @@
+package com.tistory.povia.mongo.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class PersonDto {
+	private String name;
+	private String job;
+}
